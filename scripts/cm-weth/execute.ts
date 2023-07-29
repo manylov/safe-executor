79da58a8e31b2ef8860e019054006227c6e956b1
Maxim Manylov <manylov@gmail.com>
2023-07-29 18:58:43 +0600

    add phantom mtxs

--- scripts/cm-weth/execute.ts
+++ scripts/cm-weth/execute.ts
@@ -1,9 +1,18 @@
 import { TxManager } from "../../src/base/TxManager";
 import { evm_fast_forward } from "../../src/base/utils/warp";
 
-const warpDays = 3;
+const warpDays = 4;
 
 const main = async () => {
+  console.log("QUEUE ADDITIONAL PHANTOM TOKEN TX");
+  const txManagerPhantom = new TxManager({
+    dir: "cm-weth/phantom",
+    clear: false, // use txs generated
+  });
+
+  await txManagerPhantom.callTxsFromSafeImpersonatedAccount("queue");
+
+  console.log("FAST FORWARD EVM TO 4 days");
   await evm_fast_forward(warpDays);
 
   const txManager = new TxManager({
@@ -11,7 +20,13 @@ const main = async () => {
     clear: false, // use txs generated
   });
 
+  console.log("EXECUTE CM WETH SETUP TXS");
   await txManager.callTxsFromSafeImpersonatedAccount("execute");
+
+  console.log("EXECUTE ADDITIONAL PHANTOM TOKEN TX");
+  await txManagerPhantom.callTxsFromSafeImpersonatedAccount("execute");
+
+  console.log("DONE");
 };
 
 main();
